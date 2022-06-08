ae1cd2e83556593a68511049c86d69cad949a5c8
KHDimov18 <62810926+KHDimov18@users.noreply.github.com>
2022-06-08 17:19:21 +0300

    add application properties utils

---
+++ src/main/java/utils/ApplicationProperties.java
@@ -0,0 +1,7 @@
+package utils;
+
+public class ApplicationProperties {
+    public static final String JDBC_URL = "jdbc:sqlserver://DESKTOP-QLPTQPU;databaseName=BikeShop;encrypt=true;trustServerCertificate=true;integratedSecurity=true;";
+    public static final String USERNAME = "DESKTOP-QLPTQPU\\Hello";
+    public static final String PASSWORD = "";
+}
